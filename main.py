3686eb66b298823de7c8e42484515dc65a5bf933
Jorge García LIS <jorge.garcia@lisdatasolutions.com>
2026-07-02 12:50:29 +0200

    feat(explora): implementar metodo explora (impl 2)

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ from astronomia.planetas import planetas
 from astronomia.galaxias import galaxias
 
 
+def explora(a):
+    sitios_a_no_explorar = ["los baños", "el granero", "la madriguera"]
+    print(f"el alien {a} no va a explorar {random.choice(sitios_a_no_explorar)}")
+
+
 def cosmos(a, p, g):
     for alien in a:
         print(
             f"El {alien} del planeta {random.choce(p)} de la galaxia {random.choice(g)}"
         )
+        explora(a)
 
 
 """
